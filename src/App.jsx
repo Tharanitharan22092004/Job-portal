713ed11214c29af661c11fcabff00bd8e9d508bd
Tharanitharan22092004 <tharanitharan.n2022cse@sece.ac.in>
2024-12-16 15:23:23 +0530

    Defining basic layouts of application

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,71 @@
-import './App.css'
-import { Button } from './components/ui/button'
-function App() {
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
+import AppLayout from "./layouts/app-layout";
+
+import LandingPage from "./pages/landing";
+import Onboarding from "./pages/onboarding";
+import PostJob from "./pages/post-job";
+import JobListing from "./pages/jobListing";
+import MyJobs from "./pages/my-jobs";
+import SavedJobs from "./pages/saved-jobs";
+import JobPage from "./pages/job";
+
+import "./App.css";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/onboarding",
+        element: (
+            <Onboarding />
+        ),
+      },
+      {
+        path: "/jobs",
+        element: (
+    
+            <JobListing />
+    
+        ),
+      },
+      {
+        path: "/post-job",
+        element: (
+            <PostJob />
+        ),
+      },
+      {
+        path: "/my-jobs",
+        element: (
+            <MyJobs />
+        ),
+      },
+      {
+        path: "/saved-jobs",
+        element: (
+            <SavedJobs />
+        ),
+      },
+      {
+        path: "/job/:id",
+        element: (
+            <JobPage />
+        ),
+      },
+    ],
+  },
+]);
+
+function App() {
   return (
-    <>
-    <Button>Job portal</Button>
-    </>
-  )
+      <RouterProvider router={router} />
+  );
 }
 
-export default App
+export default App;
